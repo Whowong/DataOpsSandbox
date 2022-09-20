94ec783ff1e221d131cb0f7ef62ad6eea4b11875
Whowong <54148527+Whowong@users.noreply.github.com>
2022-09-19 17:27:13 -0700

    Adding maiden name

--- dbo/Tables/Customer.sql
+++ dbo/Tables/Customer.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[Customer] (
     [FirstName] NCHAR (10) NULL,
     [LastName]  NCHAR (10) NULL, 
-    [MiddleName] NCHAR (10) NULL
+    [MiddleName] NCHAR (10) NULL,
+    [MaidenName] NCHAR (10) NULL
 );
 
 
